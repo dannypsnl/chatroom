4414850e1244374c0abc57c86581d42819498990
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-08-03 23:27:58 +0800

    cleanup

--- lib/chatroom.ex
+++ lib/chatroom.ex
@@ -2,17 +2,4 @@ defmodule Chatroom do
   @moduledoc """
   Documentation for `Chatroom`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Chatroom.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
